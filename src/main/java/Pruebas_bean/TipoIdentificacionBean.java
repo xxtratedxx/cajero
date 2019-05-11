59bc25077c368098a2bb108291e26c5089881ce2
jhonsteven2694 <jhonsteven2694@gmail.com>
2019-05-11 18:33:07 -0500

    primer pantalla funcional create tipo identificacion

---
+++ src/main/java/Pruebas_bean/TipoIdentificacionBean.java
@@ -0,0 +1,48 @@
+package Pruebas_bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import org.hibernate.Session;
+
+import co.edu.uniajc.cajero.model.TipoIdentificacion;
+import co.edu.uniajc.cajero.service.TipoIdentificacionService;
+import co.edu.uniajc.cajero.util.HibernateUtil;
+
+@ManagedBean(name = "tipoIdentificacionBean", eager = true)
+@RequestScoped
+public class TipoIdentificacionBean {
+
+	private String descripcion;
+	
+	private TipoIdentificacionService tipoIdentificacionService;
+	
+	public TipoIdentificacionBean() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		TipoIdentificacionService TipoIdentificacionService = new TipoIdentificacionService(session);
+		tipoIdentificacionService = new TipoIdentificacionService(session);
+		TipoIdentificacionService.closeSession();
+	}
+
+	public void buttonAction() {
+		
+		tipoIdentificacionService.save(new TipoIdentificacion("112345678",null));
+		addMessage("Datos de usuario enviados!! Descripcion: " + descripcion);
+		
+	}
+	
+	public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+}
