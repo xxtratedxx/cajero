aaaddd42741a2ebdce84af2bc2d0a025b61b0787
jhonsteven2694 <jhonsteven2694@gmail.com>
2019-05-13 21:18:31 -0500

    vista crear y consultar usuarios testeada

--- src/main/java/Pruebas_bean/UserBean.java
+++ src/main/java/Pruebas_bean/UserBean.java
@@ -1,6 +1,9 @@
 package Pruebas_bean;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -8,7 +11,13 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 
+import org.hibernate.Session;
+
+import co.edu.uniajc.cajero.model.TipoIdentificacion;
 import co.edu.uniajc.cajero.model.Usuario;
+import co.edu.uniajc.cajero.service.TipoIdentificacionService;
+import co.edu.uniajc.cajero.service.UsuarioService;
+import co.edu.uniajc.cajero.util.HibernateUtil;
 
 @ManagedBean(name = "userBean", eager = true)
 @RequestScoped
@@ -26,16 +35,30 @@ public class UserBean {
 	
 	public UserBean() {
 		lstUsers = new ArrayList<>();
+		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		UsuarioService usuarioService = new UsuarioService(session);
+		usuarioService = new UsuarioService(session);
+		lstUsers = usuarioService.findByIdall();
+		usuarioService.closeSession();
 	}
 	
 	public void handleKeyEvent() {
         nombre = nombre.toUpperCase();
     }
 	
-	public void buttonAction() {
+	public void buttonAction() throws ParseException {
+		
+		Date date1=new SimpleDateFormat("dd/MM/yyyy").parse("15/04/2019");
+		Integer identificacion = 4;
+		TipoIdentificacion tipoIdentificacion = new TipoIdentificacion(identificacion);
 		
 		addMessage("Datos de usuario enviados!! tipo identificacion: " + tipoIdentificacion + " | identificacion:  " + identificacion + " | nombre: " + nombre + " | apellido: " + apellido + " | direccion: " + direccion + " | celular: " + celular + " | email: " + email);
-		
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		UsuarioService usuarioService = new UsuarioService(session);
+		usuarioService = new UsuarioService(session);
+		usuarioService.save(new Usuario(tipoIdentificacion, "3837494", "prueba", "prueba", "carrera 7", "31525", "buitrago500", date1, date1));
+		usuarioService.closeSession();
     }
 	
 	public void addMessage(String summary) {
---
+++ target/generated-sources/annotations/co/edu/uniajc/cajero/model/DetalleId_.java
@@ -0,0 +1,15 @@
+package co.edu.uniajc.cajero.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(DetalleId.class)
+public abstract class DetalleId_ {
+
+	public static volatile SingularAttribute<DetalleId, Integer> idTransaccion;
+	public static volatile SingularAttribute<DetalleId, Integer> idDetalle;
+
+}
+
